cb6d4118fab7d4d91c903e2b1e5a1dbb770b326a
joadahl <72013038+joadahl@users.noreply.github.com>
2020-11-01 14:47:36 +0100

    tja

---
+++ vae/data.py
@@ -0,0 +1,75 @@
+import numpy as np
+import tensorflow as tf
+from sklearn.utils import shuffle
+
+class dsprite:
+    def __init__(self):
+        self.x, self.y, self.x_train, self.x_test, self.x_train_gen, self.x_test_gen = self.init_data()
+        self.gen_factor_size = [1, 3, 6, 40, 32, 32] #har ändar här
+
+    #"User/Desktop"
+    def init_data(self):
+        data = np.load("dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz") #"Midgard/home/joadahl/Workplace/dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz"
+        x = data['imgs']
+        y = data['latents_classes']
+        x = x.reshape((x.shape[0], 64, 64, 1))
+        x, y = shuffle(x, y)
+        x_train, x_test = np.split(x, [int(.6 * len(x))])
+        y_train, y_test = np.split(y, [int(.6 * len(x))]) #måste lägga till y_train och y_test för batch_generation tror jag
+        #x_train = x_train[0:1500] #denna bör teas bort sen
+        #x_test = x_test[0:1500]
+        return x, y, x_train, x_test, tf.data.Dataset.from_tensor_slices((x_train, x_train)).batch(64), tf.data.Dataset.from_tensor_slices((x_test, x_test)).batch(64)
+
+
+    def batch_generation_dci(self):
+        while True:
+            random_index = np.random.randint(low=1, high=len(self.gen_factor_size))
+            ran_unique_feature_ind = np.random.randint(self.gen_factor_size[random_index])
+            ind_fixed_feature = np.where(self.y[:, random_index] == ran_unique_feature_ind)
+            y_fixed_feature = self.y[ind_fixed_feature[0]]
+            x_fixed_feature = self.x[ind_fixed_feature[0]]
+            ran_values = np.random.randint(low=0, high=y_fixed_feature.shape[0], size=1)
+            y = y_fixed_feature[ran_values]
+            x_1 = x_fixed_feature[ran_values]
+            x_1 = x_1.reshape((1, 64, 64, 1))
+            yield (x_1, y)
+
+
+    def batch_generation_beta(self, L):
+        while True:
+            random_index = np.random.randint(low=1, high=len(self.gen_factor_size))
+            y = np.zeros((1, len(self.gen_factor_size)))
+            y[:, random_index] = 1
+            ran_unique_feature_ind = np.random.randint(self.gen_factor_size[random_index])
+            ind_fixed_feature = np.where(self.y[:, random_index] == ran_unique_feature_ind)
+            x_fixed_feature = self.x[ind_fixed_feature[0]]
+            rand_pick_1 = np.random.randint(low=0, high=x_fixed_feature.shape[0], size=L)
+            x_1 = x_fixed_feature[rand_pick_1]
+            rand_pick_2 = np.random.randint(low=0, high=x_fixed_feature.shape[0], size=L)
+            x_2 = x_fixed_feature[rand_pick_2]
+            yield (x_1, x_2, y)
+
+    def batch_generation_factor(self, L):
+        while True:
+            y = np.random.randint(low=1, high=len(self.gen_factor_size))
+            ran_unique_feature_ind = np.random.randint(self.gen_factor_size[y])
+            ind_fixed_feature = np.where(self.y[:, y] == ran_unique_feature_ind)
+            x_fixed_feature = self.x[ind_fixed_feature[0]]
+            rand_pick_1 = np.random.randint(low=0, high=x_fixed_feature.shape[0], size=L)
+            x_1 = x_fixed_feature[rand_pick_1]
+            yield (x_1, y)
+
+    def batch_generation_beta_2(self, L):
+        while True:
+            random_index = np.random.randint(low=1, high=len(self.gen_factor_size))
+            #y = np.zeros((1, len(self.gen_factor_size)))
+            ran_unique_feature_ind = np.random.randint(self.gen_factor_size[random_index])
+            ind_fixed_feature = np.where(self.y[:, random_index] == ran_unique_feature_ind)
+            x_fixed_feature = self.x[ind_fixed_feature[0]]
+            rand_pick_1 = np.random.randint(low=0, high=x_fixed_feature.shape[0], size=L)
+            x_1 = x_fixed_feature[rand_pick_1]
+            rand_pick_2 = np.random.randint(low=0, high=x_fixed_feature.shape[0], size=L)
+            x_2 = x_fixed_feature[rand_pick_2]
+            yield (x_1, x_2, random_index)
+
+
---
+++ vae/decoder.py
@@ -0,0 +1,38 @@
+from tensorflow import keras
+from keras.layers import Dense, Reshape, Conv2DTranspose, BatchNormalization
+import tensorflow as tf
+
+
+class decoder(keras.layers.Layer):
+
+    def __init__(self):
+        super(decoder, self).__init__()
+        self.layer_1 = Dense(units=128, activation='relu')
+        self.layer_2 = Dense(units=1024, activation='relu')
+        self.layer_3 = Conv2DTranspose(filters=64, kernel_size=(4, 4), activation='relu', strides=2, padding='same')
+        self.layer_4 = Conv2DTranspose(filters=32, kernel_size=(4, 4), activation='relu', strides=2, padding='same')
+        self.layer_5 = Conv2DTranspose(filters=32, kernel_size=(4, 4), activation='relu', strides=2, padding='same')
+        self.layer_6 = Conv2DTranspose(filters=1, kernel_size=(4, 4), activation='sigmoid', strides=2, padding='same')
+        self.reshape = Reshape((4, 4, 64))
+        self.batch_norm_1 = BatchNormalization()
+        self.batch_norm_2 = BatchNormalization()
+        self.batch_norm_3 = BatchNormalization()
+        self.batch_norm_4 = BatchNormalization()
+        self.batch_norm_5 = BatchNormalization()
+
+
+    def call(self, inputs, training=False):
+        inputs = tf.cast(inputs, dtype=tf.float32)
+        x_rec = self.layer_1(inputs)
+        x_rec = self.batch_norm_1(x_rec)
+        x_rec = self.layer_2(x_rec)
+        x_rec = self.batch_norm_2(x_rec)
+        x_rec = self.reshape(x_rec)
+        x_rec = self.layer_3(x_rec)
+        x_rec = self.batch_norm_3(x_rec)
+        x_rec = self.layer_4(x_rec)
+        x_rec = self.batch_norm_4(x_rec)
+        x_rec = self.layer_5(x_rec)
+        x_rec = self.batch_norm_5(x_rec)
+        x_rec = self.layer_6(x_rec)
+        return x_rec
